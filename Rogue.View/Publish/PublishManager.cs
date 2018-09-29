86923df975775b31596f15b21c20f1f719e85a7d
anetegithub <anete.anetes@gmail.com>
2018-09-30 02:19:11 +0300

    * создание персонажа

---
+++ Rogue.View/Publish/PublishManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Rogue.View.Publish
+{
+    using System.Collections.Generic;
+    using Rogue.View.Interfaces;
+
+    public static class PublishManager
+    {
+        private static readonly List<IPublisher> publishers = new List<IPublisher>();
+
+        private static IPublisher Current;
+
+        public static void Set(IPublisher publisher)
+        {
+            if(!publishers.Contains(publisher))
+            {
+                publishers.Add(publisher);
+            }
+
+            Current = publisher;
+        }
+
+        public static void Publish(List<IDrawSession> drawSessions)
+        {
+            Current.Publish(drawSessions);
+        }
+    }
+}
