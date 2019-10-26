6071c9f409feae205d210c9e3776e372f5db34db
anetegithub <anete.anetes@gmail.com>
2019-10-26 14:46:52 +0300

    * Время выделено в отдельную сборку

---
+++ Rogue.Time/Time.cs
@@ -0,0 +1,21 @@
+﻿namespace Rogue
+{
+    public class Time
+    {
+        public int Hours { get; private set; } = 0;
+
+        public int Minutes { get; private set; } = 0;
+
+        public int Days { get; private set; } = 0;
+
+        public int Years { get; private set; } = 0;
+
+        internal Time(int hours = 0, int minutes = 0, int days = 0, int years = 0)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Days = days;
+            Years = years;
+        }
+    }
+}
