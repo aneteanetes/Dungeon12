30a662a80a9e22fa9276a75961821caa1bb39692
a.tretyakov <a.tretyakov@bars-open.ru>
2019-11-12 17:32:01 +0300

    * Карты стража

--- Dungeon12/CardGame/Entities/Deck.cs
+++ Dungeon12/CardGame/Entities/Deck.cs
@@ -1,6 +1,4 @@
-﻿using Dungeon;
-using Dungeon.Entities;
-using Dungeon.Map.Infrastructure;
+﻿using Dungeon.Entities;
 using Dungeon12.CardGame.Entities;
 using Dungeon12.Database.CardGameDeck;
 using System.Collections.Generic;
@@ -8,8 +6,41 @@ using System.Linq;
 
 namespace Dungeon12.CardGame.Engine
 {
+
     public class Deck : DataEntity<Deck,CardGameDeckData>
     {
+        /// <summary>
+        /// <para>
+        /// 3 карты существ 1 уровня (если есть)
+        /// </para>
+        /// <para>
+        /// 2 карты существ 2 уровня (если есть)
+        /// </para>
+        /// <para>
+        /// 2 карты существ 3 уровня (если есть)
+        /// </para>
+        /// <para>
+        /// 4 карты навыка левой кнопки мыши
+        /// </para>
+        /// <para>
+        /// 3 карты навыка правой кнопки мыши
+        /// </para>
+        /// <para>
+        /// 2 карты навыка Q
+        /// </para>
+        /// <para>
+        /// 1 карта навыка E
+        /// </para>
+        /// <para>
+        /// 3 общих карт урона
+        /// </para>
+        /// <para>
+        /// 3 общих карт существ 1, 2 и 3 уровня
+        /// </para>
+        /// <para>
+        /// 3 общих карт приёмов
+        /// </para>
+        /// </summary>
         public IEnumerable<Card> Cards { get; set; }
 
         protected override void Init(CardGameDeckData dataClass)
