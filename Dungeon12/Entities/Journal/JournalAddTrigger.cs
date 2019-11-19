171f384f09e0993fb5800a5c7c24b46d361fc29c
a.tretyakov <a.tretyakov@bars-open.ru>
2019-11-19 17:28:24 +0300

    * Добавление реплик в журнал через триггеры

---
+++ Dungeon12/Entities/Journal/JournalAddTrigger.cs
@@ -0,0 +1,26 @@
+﻿using Dungeon;
+using Dungeon.Conversations;
+using Dungeon.Drawing;
+using Dungeon.Drawing.SceneObjects.Map;
+using Dungeon.Map;
+using Dungeon.View.Interfaces;
+using Dungeon12.Entites.Journal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dungeon12.Entities.Journal
+{
+    public class JournalAddTrigger : IConversationTrigger
+    {
+        public IDrawText Trigger(PlayerSceneObject arg1, GameMap arg2, string[] arg3)
+        {
+            var category = arg3[0];
+            var filter = arg3[1];
+            var cat = arg1.Avatar.Character.As<Dungeon12Class>().Journal.GetPropertyExpr<List<JournalEntry>>(category);
+            cat.AddRange(JournalEntry.LoadAll(x => x.IdentifyName.Contains(filter)));
+
+            return new DrawText("Добавлены записи в журнал").Montserrat();
+        }
+    }
+}
