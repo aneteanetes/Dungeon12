e960aff4a80c825e2557835a21bbd2d29cbf221f
anete.anetes <anete.anetes@gmail.com>
2022-10-26 22:54:12 +0300

    * focus test

--- Dungeon12/SceneObjects/World/WorldTileSceneObject.cs
+++ Dungeon12/SceneObjects/World/WorldTileSceneObject.cs
@@ -1,25 +1,44 @@
-﻿using Dungeon.Drawing;
+﻿using Dungeon;
+using Dungeon.Drawing;
 using Dungeon.Drawing.SceneObjects;
+using Dungeon.SceneObjects;
 using Dungeon.Types;
 
 namespace Dungeon12.SceneObjects.World
 {
-    public class WorldTileSceneObject : ImageSceneObject
+    public class WorldTileSceneObject : EmptySceneControl
     {
         public override bool Updatable => false;
+
+        private TextObject t;
 
         public WorldTileSceneObject()
         {
             //Color = DrawColor.DarkBlue;
             Width = WorldSettings.cellSize;
             Height = WorldSettings.cellSize;
-            Image = "terrain.png";
+            this.Image = "terrain.png";
             
             ImageRegion = new Square()
             {
                 Width = 32,
                 Height = 32
             };
+
+            t = this.AddTextCenter("F".AsDrawText().InColor(System.ConsoleColor.Red).SegoeUIBold());
+            t.Visible=false;
+        }
+
+        public override void Focus()
+        {
+            this.t.Visible=true;
+            base.Focus();
+        }
+
+        public override void Unfocus()
+        {
+            this.t.Visible=false;
+            base.Unfocus();
         }
 
         public int x { get; set; }
